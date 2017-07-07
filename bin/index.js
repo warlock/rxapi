6f8cd6fd8014318821c7fe20eafa4ac665e830fd
Josep Subils <josep@spellbook.io>
2017-07-07 14:46:40 +0200

    [update] preparing new version

--- bin/index.js
+++ bin/index.js
@@ -1,87 +1,49 @@
 #!/usr/bin/env node
 const gitclone = require('gitclone')
-const sb = require('spellbook')
-const arg = require('argcon')
+const pro = require('commander')
 const pkg = require('../package.json')
+const chalk = require('chalk')
+const exec = require("child_process").exec
+const welcome = folder => `
 
-var help = () => {
-  console.log(`
-RxApi ${pkg.version} - ${pkg.description}
-Usage:
-  new project    Generate new project in new folder
-  run            Run API server
-  help           Show this message
+  ${chalk.blue('RxAPi: ', chalk.green(pkg.version))} Instaled! Welcome!
+  ------------------------  
 
-
-Documentation:
-https://warlock.gitbooks.io/rxapi/content
-
-${pkg.license} License - Josep Subils Rigau <josep@spellbook.io>
-`)
-}
-
-arg.on('new', res => {
-  if (!sb.chain(res).get('0').isEmpty().value() && sb.isString(res[0])) {
-    console.log(`RxApi: Start downloading project in '${res[0]}' folder.`)
-    gitclone('warlock/api-template', { dest: res[0] }, () => {
-      console.log(`RxApi: Successful download in '${res[0]}' folder`)
-      const spawn = require('child_process').spawn
-      const ex = spawn('npm', ['i'], { cwd : `./${res[0]}/`})
-
-      ex.stdout.on('data', data => {
-        process.stdout.write(`${data}`)
-        console.log(`
-
-  RxApi Instaled! Welcome!
-  ------------------------
-
-  Principal documents:
-  ./${res[0]}/scaffold/http.js     API REST Generator
-  ./${res[0]}/scaffold/socket.js   WEBSOCKET Generator
-  ./${res[0]}/schema.json          JSON Data model schema
-  ./${res[0]}/conf.json            Server configuration file
-
-  ------------------------
-
-  Development documentation:
+  Documentation:
   https://warlock.gitbooks.io/rxapi/content
 
   ------------------------
 
-  Please 'cd ${res[0]}' and run server using 'rxapi run' or 'npm start'
+  Please 'cd ${folder}' and run server using 'rxapi run' or 'npm start'
 
-`)
-      })
+`
 
-      ex.stderr.on('data', data => {
-        process.stdout.write(`${data}`)
-      })
+pro
+.version(
+  chalk.blue('RxAPi: ', chalk.green(pkg.version)) + '\n'
+)
 
-      ex.on('close', code => {
-      })
+pro
+.command('new <project>')
+.action(project => {
+  console.log(chalk.blue('RxAPi: ', chalk.green(pkg.version)))
+  console.log(`Start download in '${project}' folder.`)
+  gitclone('warlock/api-template', { dest: project }, () => {
+    exec(`cd project && npm i`, (err, stdout, stderr) => {
+      if (err) console.error(err)
+      else {
+        console.log(welcome())
+      }
     })
-  } else console.log('Pease include the name of project or directory')
-})
-
-arg.on('help', () => {
-  help()
-})
-
-arg.on('run', () => {
-  const spawn = require('child_process').spawn
-  const ex = spawn('npm', ['start'])
-  ex.stdout.on('data', (data) => {
-    process.stdout.write(`${data}`)
-  })
-
-  ex.stderr.on('data', (data) => {
-    process.stderr.write(`${data}`)
-  })
-
-  ex.on('close', (code) => {
   })
 })
 
-arg.alone(res => {
-  help()
+pro
+.command('run')
+.action(() => {
+  exec("npm start", (err, stdout, stderr) => {
+    if (err) console.error(err)
+  })
 })
+
+pro.parse(process.argv)
